3d6dbcbdcacdb9c79c844619d68e1fd5846453d0
Greg Anderson <greg@infobytes.com>
2015-10-30 14:47:43 +0000

    Now count points for a specific set of words.

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ words.add('we')
 words.add('our')
 words.add('ours')
 
+# Take a tweet and map the user to a list of points
 def split_words(s):
-	word_list = s.split(' ')
-	return map(lambda x: 1 if x in words else 0, word_list)
+	word_list = s['text'].split(' ')
+	return (s['user'].asDict()['screen_name'], map(lambda x: 1 if x in words else 0, word_list))
 
-a = f.map(lambda x: x.asDict()['text'])
-b = a.filter(lambda x: x != None)
-c = b.map(split_words)
-d = c.map(lambda lst: reduce(lambda x, y: x+y, lst))
-
+a = f.map(lambda x: x.asDict())				# Turn JSON output into dictionary
+b = a.filter(lambda x: x['text'] != None)	# Filter out non-tweets
+c = b.map(split_words)						# Map user to a list of points for each tweet
+d = c.map(lambda lst: (lst[0], reduce(lambda x, y: x+y, lst[1])))	# Add up points for each tweet
+e = d.reduceByKey(lambda x, y: x+y)			# Gather all of the scores for each user
+g = e.filter(lambda x: x[1] > 3)			# Show only those that had more points than 3
