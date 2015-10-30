c30f54fcc5208c930a2744fa0ac0e3b49c99892c
Greg Anderson <greg@infobytes.com>
2015-10-30 14:29:05 +0000

    Now give points for specific words.

--- main.py
+++ main.py
@@ -5,5 +5,21 @@ filename = 'output.txt'
 sqlContext = SQLContext(sc)
 f = sqlContext.jsonFile(filename)
 
-a = f.map(lambda x: x.asDict())
-b = a.filter(lambda x: x['text'] != None)
+words = set()
+words.add('I')
+words.add('I\'m')
+words.add('my')
+words.add('mine')
+words.add('we')
+words.add('our')
+words.add('ours')
+
+def split_words(s):
+	word_list = s.split(' ')
+	return map(lambda x: 1 if x in words else 0, word_list)
+
+a = f.map(lambda x: x.asDict()['text'])
+b = a.filter(lambda x: x != None)
+c = b.map(split_words)
+d = c.map(lambda lst: reduce(lambda x, y: x+y, lst))
+
