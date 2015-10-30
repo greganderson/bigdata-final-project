bc26b9c10a8c07a78148753c7aef191421a9d438
Greg Anderson <greg@infobytes.com>
2015-10-30 20:36:04 +0000

    Genericized the point function to take lists of words

--- main.py
+++ main.py
@@ -5,23 +5,19 @@ filename = 'output.txt'
 sqlContext = SQLContext(sc)
 f = sqlContext.jsonFile(filename)
 
-words = set()
-words.add('I')
-words.add('I\'m')
-words.add('my')
-words.add('mine')
-words.add('we')
-words.add('our')
-words.add('ours')
+narcissism = set(['i', 'i\'m', 'me', 'my', 'mine', 'we', 'our', 'ours', 'we\'re'])
+neuroticism = set(['scared', 'worried', 'frustrated'])
 
 # Take a tweet and map the user to a list of points
-def split_words(s):
+def split_words(s, words):
 	word_list = s['text'].split(' ')
-	return (s['user'].asDict()['screen_name'], map(lambda x: 1 if x in words else 0, word_list))
+	return (s['user'].asDict()['screen_name'], map(lambda x: 1 if x.lower() in words else 0, word_list))
 
-a = f.map(lambda x: x.asDict())				# Turn JSON output into dictionary
-b = a.filter(lambda x: x['text'] != None)	# Filter out non-tweets
-c = b.map(split_words)						# Map user to a list of points for each tweet
+a = f.map(lambda x: x.asDict())					# Turn JSON output into dictionary
+b = a.filter(lambda x: x['text'] != None)		# Filter out non-tweets
+
+# Narcissism
+c = b.map(lambda x: split_words(x, narcissism))	# Map user to a list of points for each tweet
 d = c.map(lambda lst: (lst[0], reduce(lambda x, y: x+y, lst[1])))	# Add up points for each tweet
-e = d.reduceByKey(lambda x, y: x+y)			# Gather all of the scores for each user
-g = e.filter(lambda x: x[1] > 3)			# Show only those that had more points than 3
+e = d.reduceByKey(lambda x, y: x+y)				# Gather all of the scores for each user
+g = e.filter(lambda x: x[1] > 0)				# Show only those that had more points than 3
