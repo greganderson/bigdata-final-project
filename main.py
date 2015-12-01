134fbd3b891facba114759563b178f17de0d8005
Greg Anderson <greg@infobytes.com>
2015-12-01 09:08:51 +0000

    Now computes score for all tweets.

--- main.py
+++ main.py
@@ -26,20 +26,16 @@ def map_to_count(s):
 			result_d[word] = 1
 	# TODO: Is this the best way to get the coordinates?
 	#return (s['coordinates'], result_d)
-	return (s['place'].asDict()['bounding_box'].asDict()['coordinates'][0][0], result_d)
+	return (s['coordinates'].asDict()['coordinates'], result_d)
+	#return (s['place'].asDict()['bounding_box'].asDict()['coordinates'][0][0], result_d)
 
 
 a = f.map(lambda x: x.asDict())				# Turn JSON output into dictionary
 b = a.filter(lambda x: x['text'] != None)		# Filter out non-tweets
 
 
-### 
-### TODO This code just needs to be repeated for each category.  Not sure how to do that yet though
-###	
-
-# Compute the rating for an individual tweet
-loc_n_text =  map_to_count(b.first())
+# Store {location : tweet text} for all tweets
 loc_n_text = b.map(map_to_count)
 
-print (loc_n_text[0], rate_tweet(loc_n_text[1]))
-
+# Store {location : score} for all tweets
+loc_n_score = loc_n_text.map(lambda x: (x[0], rate_tweet(x[1])))
